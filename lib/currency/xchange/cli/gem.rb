1bb498d5c665c68ed7541678b22864ceb2f5501c
Jake <jakemalto@yahoo.com>
2016-03-05 09:40:45 +0800

    Created bundler gem files

---
+++ lib/currency/xchange/cli/gem.rb
@@ -0,0 +1,11 @@
+require "currency/xchange/cli/gem/version"
+
+module Currency
+  module Xchange
+    module Cli
+      module Gem
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/currency/xchange/cli/gem/version.rb
@@ -0,0 +1,9 @@
+module Currency
+  module Xchange
+    module Cli
+      module Gem
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
