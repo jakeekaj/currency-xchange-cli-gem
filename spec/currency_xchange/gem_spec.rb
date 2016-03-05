8ae1be2fe1beb5ad9ab6c6056b5876e125602383
Jake <jakemalto@yahoo.com>
2016-03-05 17:23:38 +0800

    fixed some more paths in original rspec

--- spec/currency_xchange/gem_spec.rb
+++ spec/currency_xchange/gem_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe Currency::Xchange::Cli::Gem do
+describe CurrencyXchange::CLI
   it 'has a version number' do
     expect(Currency::Xchange::Cli::Gem::VERSION).not_to be nil
   end
 
   it 'does something useful' do
-    expect(false).to eq(true)
-  end
+   expect(false).to eq(true)
+ end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'currency/xchange/cli/gem'
+require 'currency_xchange/cli'
+require 'currency_xchange/scraper'
