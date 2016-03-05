2fdeb823d2d23e31a0e07063fd6e1288468ed389
Jake <jakemalto@yahoo.com>
2016-03-05 13:37:35 +0800

    fixed CLI functionality - again method added

--- lib/currency_xchange/cli.rb
+++ lib/currency_xchange/cli.rb
@@ -2,7 +2,7 @@ class CurrencyXchange::CLI
 
 
   def start
-    puts "\n <<<<<<<<     CURRENCY X-CHANGE    >>>>>>>>\n "
+    puts "\n <<<<<<<<        CURRENCY X-CHANGE         >>>>>>>>\n "
     puts "\n <<<<<<<<    LIVE RATES from x-rates.com   >>>>>>>>\n "
     puts "You can choose from the following, or search for your local currency in our directory"
     puts " \n___________________________________________________________"
@@ -16,9 +16,38 @@ class CurrencyXchange::CLI
     CurrencyXchange::Scraper.increase_amount
     CurrencyXchange::Scraper.process_2nd_input
     CurrencyXchange::Scraper.convert
-    CurrencyXchange::Scraper.again?
+    again
   end
 
+  def again
+  	puts " \n-------------------------------------"
+    puts "Awesome. What do you want to do next? "
+    puts "-------------------------------------"
+    puts " \nWould you like to convert to another currency?            - enter 1"
+    puts "Would you like to select a new currency?                  - enter 2"
+    puts "Would you like to change the amount you want to convert?  - enter 3"
+    puts "Enter any key to terminate the program:"
+    
+    key = gets.chomp
+    if key == "1"
+      CurrencyXchange::Scraper.process_2nd_input
+      CurrencyXchange::Scraper.convert
+      self.again
+    elsif key == "2"
+      puts "Enter your currency or search through our list"
+      CurrencyXchange::Scraper.process_main_input
+      CurrencyXchange::Scraper.increase_amount
+      CurrencyXchange::Scraper.process_2nd_input
+      CurrencyXchange::Scraper.convert
+      self.again
+    elsif key == "3"
+      CurrencyXchange::Scraper.increase_amount
+      CurrencyXchange::Scraper.convert
+      self.again
+    else
+      puts "Thank you for using 'Currency X-Change' Goodbye!"
+    end
+  end
 
 end
  
--- lib/currency_xchange/scraper.rb
+++ lib/currency_xchange/scraper.rb
@@ -1,122 +1,110 @@
 class CurrencyXchange::Scraper
 
-@@list = []
-@@list2 = []
-@@hash = {}
-@@hash2 = {}
-@@currency = nil
-@@currency2 = nil
-@@amount = 1.0
+  @@list = []
+  @@hash = {}
+  @@hash2 = {}
+  @@hash3 = {}
+  @@currency = nil
+  @@currency2 = nil
+  @@amount = 1.0
 
-def self.scrape_index
-  html = open("http://www.x-rates.com/")
-  doc = Nokogiri::HTML(html)
-  doc.css("ul.currencyList li a").each do |x|
-    @@hash[x.text] = x.attr("href")
-  end
-  @@list = @@hash.keys
-end
-
-def self.process_main_input
-  input = gets.chomp
-    if input == "search"
-      self.search_list
-    elsif @@list.include?(input)
-      @@currency = input
-    else
-      puts "Invalid entry. Please try again. Enter 'search' or name of currency"
-      self.process_main_input
+  def self.scrape_index
+    html = open("http://www.x-rates.com/")
+    doc = Nokogiri::HTML(html)
+    doc.css("ul.currencyList li a").each do |x|
+      @@hash[x.text] = x.attr("href")
     end
-end  
+    @@list = @@hash.keys
+  end
 
-
-def self.search_list
-  results =[]
-  puts "Enter starting letter of currency/country:"
-  letter = gets.chomp
-  if letter =~ /[a-zA-Z]/
-    @@list.each do|currency| 
-      if currency[0] == letter.upcase
-        puts currency
-        results << currency
+  def self.process_main_input
+    input = gets.chomp
+      if input == "search"
+        self.search_list
+      elsif @@list.include?(input)
+        @@currency = input
+      else
+        puts "Invalid entry. Please try again. Enter 'search' or name of currency"
+        self.process_main_input
       end
-    end
-    if results.size == 0
-      puts "No results found."
-    end
-  else
-    puts "Invalid letter. Try again"
-     self.search_list
-  end
+  end  
 
-  puts "Do you want to search again? Y/N"
-     x = gets.chomp.upcase
-     if x == "Y"
-      search_list
-    elsif x == "N"
-      puts "Alright. Enter 'search' or name of currency"
-      self.process_main_input
+
+  def self.search_list
+    results =[]
+    puts "Enter starting letter of currency/country:"
+    letter = gets.chomp
+    if letter =~ /[a-zA-Z]/
+      @@list.each do|currency| 
+        if currency[0] == letter.upcase
+          puts currency
+          results << currency
+        end
+      end
+      if results.size == 0
+        puts "No results found."
+      end
     else
-      puts "Sorry. Invalid entry. Searching again..."
-      self.search_list
+      puts "Invalid letter. Try again"
+       self.search_list
     end
-end
+
+    puts "Do you want to search again? Y/N"
+       x = gets.chomp.upcase
+       if x == "Y"
+        search_list
+      elsif x == "N"
+        puts "Alright. Enter 'search' or name of currency"
+        self.process_main_input
+      else
+        puts "Sorry. Invalid entry. Searching again..."
+        self.search_list
+      end
+  end
 
 
-def self.increase_amount
-  puts "How many #{@@currency} do you want to convert? Commas(,) are not allowed. "
-  num = gets.chomp.to_f
-  @@amount = num
-end
+  def self.increase_amount
+    puts "How many #{@@currency} do you want to convert? Commas(,) are not allowed. "
+    num = gets.chomp.to_f
+    @@amount = num
+  end
 
-def self.process_2nd_input
-  puts "What currency do you like to convert to?"
-  answer = gets.chomp
-  if answer == @@currency
-    puts "You can't have same currencies for conversion."
-    self.process_2nd_input
-  elsif @@list.include?(answer)
+  def self.process_2nd_input
+    puts "What currency do you like to convert to?"
+    answer = gets.chomp
+    if answer == @@currency
+      puts "You can't have same currencies for conversion."
+      self.process_2nd_input
+    elsif @@list.include?(answer)
+      @@currency2 = answer
+    else
+       puts "Invalid currency. Please try again:"
+       self.process_2nd_input
+    end
+  end
+
+  def self.convert
+    html = open(@@hash[@@currency]+"&amount=#{@@amount}")#
+    doc = Nokogiri::HTML(html)
+    doc.css("table.tablesorter tbody tr").each do |x|
+      @@hash2[x.css("td")[0].text] = x.css("td")[1].text
+    end
     puts "Converting your money now..."
-    puts " >> \n >>> \n >>>> \n >>>>> \n "
-    @@currency2 = answer
-  else
-     puts "Invalid currency. Please try again:"
-     self.process_2nd_input
-  end
-end
+    puts " >> \n >>> \n >>>> \n >>>>>"
+    puts " >>>>>> Your #{@@amount} in #{@@currency} is worth #{@@hash2[@@currency2]} in #{@@currency2}!"
+    ##if the amount is more than 1, we show the base rate conversion of 1 local currency 
+    if @@amount > 1.0
+    html = open(@@hash[@@currency]+"&amount=1")#
+    doc = Nokogiri::HTML(html)
+    doc.css("table.tablesorter tbody tr").each do |x|
+      @@hash3[x.css("td")[0].text] = x.css("td")[1].text
+    end
 
-def self.convert
-  html = open(@@hash[@@currency]+"&amount=#{@@amount}")#
-  doc = Nokogiri::HTML(html)
-  doc.css("table.tablesorter tbody tr").each do |x|
-    @@hash2[x.css("td")[0].text] = x.css("td")[1].text
+    puts " \n >>>>>> 1 #{@@currency} is equal to #{@@hash3[@@currency2]} #{@@currency2}."
+    end
   end
+
   
-  puts "Your #{@@amount} in #{@@currency} is worth #{@@hash2[@@currency2]} in #{@@currency2}!"
-end
-
-def self.again?
-  puts " \n-------------------------------------"
-  puts "Awesome. What do you want to do next? "
-  puts "-------------------------------------"
-  puts " \nWould you like to convert to another currency?            - enter 1"
-  puts "Would you like to select a new currency?                  - enter 2"
-  puts "Would you like to change the amount you want to convert?  - enter 3"
-  puts "Enter any key to terminate the program:"
-  key = gets.chomp
-  if key == "1"
-    self.process_2nd_input
-  elsif key == "2"
-    puts "Enter your currency or search through our list"
-    self.process_main_input
-  elsif key == "3"
-    self.increase_amount
-    self.convert
-  else
-    puts "Thank you for using 'Currency X-Change' Goodbye!"
-  end
-end
-
 end
 
 
